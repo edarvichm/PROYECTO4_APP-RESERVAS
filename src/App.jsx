80f2ddf9340f1491f1a3a223c9929934235c88d3
edarvichm <edarvichm@gmail.com>
2023-07-05 21:48:57 -0400

    Ajuste posiciones objetos

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 // import { useState } from 'react'
-import './App.css'
-import { NavBar } from './components/NavBar'
+// import './App.css'
+import { NavBar } from "./components/NavBar";
 // import HomePage from './pages/HomePage'
-import { MainRoutes } from './routers/MainRouter'
+import { MainRoutes } from "./routers/MainRouter";
 
 function App() {
-  
-
   return (
     <>
       <NavBar />
-      <MainRoutes />
-
+      <div className="container m-5 p-3">
+        <MainRoutes />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,52 +1,106 @@
 // import React from 'react'
 
-import { NavLink } from "react-router-dom"
-
+import { NavLink } from "react-router-dom";
 
 export const NavBar = () => {
   return (
     <>
-      <nav className="navbar bg-body-tertiary fixed-top">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Offcanvas navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div className="offcanvas-body">
-        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li className="nav-item">
-            <NavLink className="nav-link active" aria-current="page" href="/">Home</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" href="#">Link</NavLink>
-          </li>
-          <li className="nav-item dropdown">
-            <NavLink className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </NavLink>
-            <ul className="dropdown-menu">
-              <li><NavLink className="dropdown-item" href="#">Action</NavLink></li>
-              <li><NavLink className="dropdown-item" href="#">Another action</NavLink></li>
-              <li>
-                <hr className="dropdown-divider" />
-              </li>
-              <li><NavLink className="dropdown-item" href="#">Something else here</NavLink></li>
-            </ul>
-          </li>
-        </ul>
-        <form className="d-flex mt-3" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </div>
-</nav>
+      <nav className="navbar  bg-body-tertiary fixed-top b-auto">
+        {/* fixed-top */}
+        <div className="container-fluid">
+          <a className="navbar-brand" to="#">
+            Offcanvas navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="offcanvas offcanvas-end"
+            tabIndex="-1"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+          >
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                Offcanvas
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li className="nav-item">
+                  <NavLink
+                    className="nav-link active"
+                    aria-current="page"
+                    to="/"
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="#">
+                    Link
+                  </NavLink>
+                </li>
+                <li className="nav-item dropdown">
+                  <NavLink
+                    className="nav-link dropdown-toggle"
+                    to="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown
+                  </NavLink>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <NavLink className="dropdown-item" to="#">
+                        Action
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink className="dropdown-item" to="#">
+                        Another action
+                      </NavLink>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <NavLink className="dropdown-item" to="#">
+                        Something else here
+                      </NavLink>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <form className="d-flex mt-3" role="search">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button className="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
-  )
-}
+  );
+};
