d872bb76c65db44b31cceb8ba786f84dd2d6ada2
edarvichm <edarvichm@gmail.com>
2023-07-08 18:57:07 -0400

    ajuste error navHeight

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from "react";
 // import './App.css'
 import { NavBar } from "./components/NavBar";
 // import HomePage from './pages/HomePage'
@@ -6,24 +6,25 @@ import { MainRoutes } from "./routers/MainRouter";
 
 function App() {
   // Calcula el tamaño del navbar dinámicamente
-  const [navHeight, setNavHeight] = useState(0); //useRef()
+  // const [navHeight, setNavHeight] = useState(0); //useRef()
   // console.log('nav '+navHeight);
   // const handleResize = () => {
   //   setNavHeight(document.getElementById("navbar").offsetHeight);
   //   console.log('nav2 '+document.getElementById("navbar").offsetHeight);
-   
+
   // };
   // window.addEventListener("resize", handleResize);
   // window.addEventListener("load", handleResize);
   // handleResize();
   // const navbarHeight = document.getElementById("navbar").offsetHeight
-  
+
   return (
     <>
       <div className="container">
-        <NavBar navHeight={setNavHeight}/>
+        {/* <NavBar navHeight={setNavHeight} /> */}
+        <NavBar />
       </div>
-      <div className="container" style={{ marginTop: navHeight}}>
+      <div className="container" style={{ marginTop: 200 }}>
         <MainRoutes />
       </div>
     </>
